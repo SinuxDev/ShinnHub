bbaa48acf8fe76afc16686b1b1db89e705e36499
Aung Ye Htet <aung.atg1738@gmail.com>
2025-03-23 07:51:09 +0630

    ProjectFlowLayoutPanenl

--- FreelancePlatform/UI/ClientProfileForm.Designer.cs
+++ FreelancePlatform/UI/ClientProfileForm.Designer.cs
@@ -28,14 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             ClientProfileCheckButton = new Guna.UI2.WinForms.Guna2GradientTileButton();
             CreatePostRedirectButton = new Guna.UI2.WinForms.Guna2GradientTileButton();
             label1 = new Label();
@@ -47,14 +43,13 @@
             DynamicClientCompany = new Label();
             DynamicClientIndustry = new Label();
             panel1 = new Panel();
-            ClientProjectDataGridViews = new Guna.UI2.WinForms.Guna2DataGridView();
+            ProjectFlowLayoutPanel = new FlowLayoutPanel();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)ClientProjectDataGridViews).BeginInit();
             SuspendLayout();
             // 
             // ClientProfileCheckButton
             // 
-            ClientProfileCheckButton.CustomizableEdges = customizableEdges1;
+            ClientProfileCheckButton.CustomizableEdges = customizableEdges17;
             ClientProfileCheckButton.DisabledState.BorderColor = Color.DarkGray;
             ClientProfileCheckButton.DisabledState.CustomBorderColor = Color.DarkGray;
             ClientProfileCheckButton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -65,14 +60,14 @@
             ClientProfileCheckButton.ForeColor = Color.Black;
             ClientProfileCheckButton.Location = new Point(51, 55);
             ClientProfileCheckButton.Name = "ClientProfileCheckButton";
-            ClientProfileCheckButton.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            ClientProfileCheckButton.ShadowDecoration.CustomizableEdges = customizableEdges18;
             ClientProfileCheckButton.Size = new Size(115, 43);
             ClientProfileCheckButton.TabIndex = 2;
             ClientProfileCheckButton.Text = "Your Profile";
             // 
             // CreatePostRedirectButton
             // 
-            CreatePostRedirectButton.CustomizableEdges = customizableEdges3;
+            CreatePostRedirectButton.CustomizableEdges = customizableEdges19;
             CreatePostRedirectButton.DisabledState.BorderColor = Color.DarkGray;
             CreatePostRedirectButton.DisabledState.CustomBorderColor = Color.DarkGray;
             CreatePostRedirectButton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -83,7 +78,7 @@
             CreatePostRedirectButton.ForeColor = Color.Black;
             CreatePostRedirectButton.Location = new Point(269, 55);
             CreatePostRedirectButton.Name = "CreatePostRedirectButton";
-            CreatePostRedirectButton.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            CreatePostRedirectButton.ShadowDecoration.CustomizableEdges = customizableEdges20;
             CreatePostRedirectButton.Size = new Size(115, 43);
             CreatePostRedirectButton.TabIndex = 3;
             CreatePostRedirectButton.Text = "Create Post";
@@ -176,79 +171,21 @@
             panel1.Size = new Size(495, 280);
             panel1.TabIndex = 12;
             // 
-            // ClientProjectDataGridViews
+            // ProjectFlowLayoutPanel
             // 
-            ClientProjectDataGridViews.AllowUserToOrderColumns = true;
-            ClientProjectDataGridViews.AllowUserToResizeRows = false;
-            dataGridViewCellStyle1.BackColor = Color.FromArgb(128, 255, 255);
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(231, 229, 255);
-            dataGridViewCellStyle1.SelectionForeColor = Color.FromArgb(71, 69, 94);
-            ClientProjectDataGridViews.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            ClientProjectDataGridViews.CellBorderStyle = DataGridViewCellBorderStyle.None;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = Color.FromArgb(100, 88, 255);
-            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle2.ForeColor = Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(100, 88, 255);
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
-            ClientProjectDataGridViews.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            ClientProjectDataGridViews.ColumnHeadersHeight = 22;
-            ClientProjectDataGridViews.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = Color.White;
-            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle3.ForeColor = Color.FromArgb(71, 69, 94);
-            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(231, 229, 255);
-            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(71, 69, 94);
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
-            ClientProjectDataGridViews.DefaultCellStyle = dataGridViewCellStyle3;
-            ClientProjectDataGridViews.GridColor = Color.FromArgb(231, 229, 255);
-            ClientProjectDataGridViews.Location = new Point(690, 87);
-            ClientProjectDataGridViews.Name = "ClientProjectDataGridViews";
-            ClientProjectDataGridViews.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = Color.White;
-            dataGridViewCellStyle4.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = Color.White;
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            ClientProjectDataGridViews.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
-            ClientProjectDataGridViews.RowHeadersVisible = false;
-            ClientProjectDataGridViews.RowHeadersWidth = 51;
-            ClientProjectDataGridViews.Size = new Size(656, 389);
-            ClientProjectDataGridViews.TabIndex = 13;
-            ClientProjectDataGridViews.ThemeStyle.AlternatingRowsStyle.BackColor = Color.FromArgb(128, 255, 255);
-            ClientProjectDataGridViews.ThemeStyle.AlternatingRowsStyle.Font = new Font("Segoe UI", 9F);
-            ClientProjectDataGridViews.ThemeStyle.AlternatingRowsStyle.ForeColor = SystemColors.ControlText;
-            ClientProjectDataGridViews.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = Color.FromArgb(231, 229, 255);
-            ClientProjectDataGridViews.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = Color.FromArgb(71, 69, 94);
-            ClientProjectDataGridViews.ThemeStyle.BackColor = Color.White;
-            ClientProjectDataGridViews.ThemeStyle.GridColor = Color.FromArgb(231, 229, 255);
-            ClientProjectDataGridViews.ThemeStyle.HeaderStyle.BackColor = Color.FromArgb(100, 88, 255);
-            ClientProjectDataGridViews.ThemeStyle.HeaderStyle.BorderStyle = DataGridViewHeaderBorderStyle.None;
-            ClientProjectDataGridViews.ThemeStyle.HeaderStyle.Font = new Font("Segoe UI", 9F);
-            ClientProjectDataGridViews.ThemeStyle.HeaderStyle.ForeColor = Color.White;
-            ClientProjectDataGridViews.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            ClientProjectDataGridViews.ThemeStyle.HeaderStyle.Height = 22;
-            ClientProjectDataGridViews.ThemeStyle.ReadOnly = false;
-            ClientProjectDataGridViews.ThemeStyle.RowsStyle.BackColor = Color.White;
-            ClientProjectDataGridViews.ThemeStyle.RowsStyle.BorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-            ClientProjectDataGridViews.ThemeStyle.RowsStyle.Font = new Font("Segoe UI", 9F);
-            ClientProjectDataGridViews.ThemeStyle.RowsStyle.ForeColor = Color.FromArgb(71, 69, 94);
-            ClientProjectDataGridViews.ThemeStyle.RowsStyle.Height = 29;
-            ClientProjectDataGridViews.ThemeStyle.RowsStyle.SelectionBackColor = Color.FromArgb(231, 229, 255);
-            ClientProjectDataGridViews.ThemeStyle.RowsStyle.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            ProjectFlowLayoutPanel.AutoScroll = true;
+            ProjectFlowLayoutPanel.Dock = DockStyle.Right;
+            ProjectFlowLayoutPanel.Location = new Point(552, 0);
+            ProjectFlowLayoutPanel.Name = "ProjectFlowLayoutPanel";
+            ProjectFlowLayoutPanel.Size = new Size(932, 660);
+            ProjectFlowLayoutPanel.TabIndex = 12;
             // 
             // ClientProfileForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1484, 660);
-            Controls.Add(ClientProjectDataGridViews);
+            Controls.Add(ProjectFlowLayoutPanel);
             Controls.Add(panel1);
             Controls.Add(CreatePostRedirectButton);
             Controls.Add(ClientProfileCheckButton);
@@ -257,7 +194,6 @@
             Load += ClientProfileForm_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)ClientProjectDataGridViews).EndInit();
             ResumeLayout(false);
         }
 
@@ -274,6 +210,6 @@
         private Label DynamicClientCompany;
         private Label DynamicClientIndustry;
         private Panel panel1;
-        private Guna.UI2.WinForms.Guna2DataGridView ClientProjectDataGridViews;
+        private FlowLayoutPanel ProjectFlowLayoutPanel;
     }
 }
--- FreelancePlatform/UI/ClientProfileForm.cs
+++ FreelancePlatform/UI/ClientProfileForm.cs
@@ -52,14 +52,81 @@ namespace FreelancePlatform.UI
         {
             List<Project> projects = projectService.GetProjectsByClientID(clientID);
 
+            ProjectFlowLayoutPanel.Controls.Clear();
+
             if (projects.Count > 0)
             {
-                ClientProjectDataGridViews.DataSource = projects;
+                foreach (var project in projects)
+                {
+                    Panel projectPanel = CreateProjectPanel(project);
+                    ProjectFlowLayoutPanel.Controls.Add(projectPanel);
+                }
             }
             else
             {
-                MessageBox.Show("No projects found for this client.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Label noProjectsLabel = new Label
+                {
+                    Text = "No projects found.",
+                    AutoSize = true,
+                    ForeColor = Color.Gray
+                };
+
+                ProjectFlowLayoutPanel.Controls.Add(noProjectsLabel);
             }
         }
+
+        private Panel CreateProjectPanel(Project project)
+        {
+            Panel panel = new Panel
+            {
+                Size = new Size(400, 150),  // Adjust size as needed
+                BorderStyle = BorderStyle.FixedSingle,
+                Padding = new Padding(10),
+                Margin = new Padding(5)
+            };
+
+            Label titleLabel = new Label
+            {
+                Text = project.ProjectTitle,
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                AutoSize = true,
+                Location = new Point(10, 10)
+            };
+
+            Label descriptionLabel = new Label
+            {
+                Text = project.ProjectDescription,
+                AutoSize = true,
+                Location = new Point(10, 40),
+                MaximumSize = new Size(380, 40)  // Wrap text if too long
+            };
+
+            Label budgetLabel = new Label
+            {
+                Text = $"Budget: ${project.ProjectBudget}",
+                Font = new Font("Arial", 10, FontStyle.Italic),
+                AutoSize = true,
+                Location = new Point(10, 90)
+            };
+
+            Button viewButton = new Button
+            {
+                Text = "View Details",
+                Size = new Size(100, 30),
+                Location = new Point(10, 120)
+            };
+
+            // Add event for View Button (if needed)
+            viewButton.Click += (sender, e) => MessageBox.Show($"Project: {project.ProjectTitle}");
+
+            // Add controls to panel
+            panel.Controls.Add(titleLabel);
+            panel.Controls.Add(descriptionLabel);
+            panel.Controls.Add(budgetLabel);
+            panel.Controls.Add(viewButton);
+
+            return panel;
+        }
+
     }
 }
