1da70bee7df2caaf3d13fac4e288ded523aff873
Aung Ye Htet <aung.atg1738@gmail.com>
2025-03-26 14:43:39 +0630

    UserModel:ExceptionHandling

--- FreelancePlatform/Models/User.cs
+++ FreelancePlatform/Models/User.cs
@@ -3,9 +3,9 @@
     public class User
     {
         public int ID { get; set; }
-        public string UserName { get; set; }
-        public string UserPassword { get; set; }
-        public string UserEmail { get; set; }
-        public string UserType { get; set; }
+        public string? UserName { get; set; }
+        public string? UserPassword { get; set; }
+        public string? UserEmail { get; set; }
+        public string? UserType { get; set; }
     }
 }
